b1994fa63beab66a99beb51efcb16f3c10556b16
n <2585803+tt5@users.noreply.github.com>
2022-07-17 01:26:24 +0200

    supabase login

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabaseClient'
+import Auth from '../components/Auth'
+import Account from '../components/Account'
 
 export default function Home() {
+
+  const [session, setSession] = useState(null)
+
+    useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+        {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+      </div>
 
       <main>
         <h1 className="title">
